4c25a5f0ba40ffcd8e4833369855c43770e4abe6
NainaSahiri <nainasahiri17@gmail.com>
2022-04-13 19:54:26 -0500

    added animal

--- app.js
+++ app.js
@@ -4,17 +4,67 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const connectionString =
+  process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var animalRouter = require('./routes/animal');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
+var resourceRouter = require('./routes/resource');
+var animal = require('./models/animal');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// We can seed the collection if needed on server start
+async function recreateDB() {
+  // Delete everything
+  await animal.deleteMany();
+
+  let instance1 = new animal({
+    animal_name: "Dog", animal_breed: "Husky",
+    animal_weight: 18
+  });
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved")
+  });
+
+  let instance2 = new
+    animal({
+      animal_name: "Monkey", animal_breed: "Chimpanzee",
+      animal_weight: 8
+    });
+  instance2.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Second object saved")
+  });
+
+  let instance3 = new
+    animal({
+      animal_name: "Cat", animal_breed: "Bengal Cat",
+      animal_weight: 15
+    });
+  instance3.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Third object saved")
+  });
+
+}
+
+let reseed = true; 
+if (reseed) { recreateDB();} 
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,14 +77,15 @@ app.use('/users', usersRouter);
 app.use('/animal', animalRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
+app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ models/animal.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose")
+const animalSchema = mongoose.Schema({
+animal_name: String,
+animal_breed: String,
+animal_weight: Number
+})
+module.exports = mongoose.model("animal",
+animalSchema)
