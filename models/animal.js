fb76739126af004a1700bfc0f990e05e164cc28c
NainaSahiri <nainasahiri17@gmail.com>
2022-04-26 14:23:29 -0500

    bonus

--- models/animal.js
+++ models/animal.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose")
 const animalSchema = mongoose.Schema({
 animal_name:String,
-animal_breed:String,
-animal_weight:Number
+animal_breed:{type:String,
+    minLength:5,
+    maxLength:20},
+animal_weight:{type:Number,
+    min:2,
+    max:1000
+}
 })
 module.exports = mongoose.model("animal",animalSchema)
--- routes/animal.js
+++ routes/animal.js
@@ -1,7 +1,15 @@
 var express = require('express');
 const animal_controlers= require('../controllers/animal');
 var router = express.Router();
-
+// A little function to check if we have an authorized user and continue on or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 /* GET home page. */
   router.get('/', animal_controlers.animal_view_all_Page );
   
@@ -10,13 +18,13 @@ var router = express.Router();
 router.get('/detail', animal_controlers.animal_view_one_Page); 
 
 /* GET create animal page */ 
-router.get('/create', animal_controlers.animal_create_Page); 
+router.get('/create',secured, animal_controlers.animal_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', animal_controlers.animal_update_Page); 
+router.get('/update',secured, animal_controlers.animal_update_Page); 
 
 /* GET delete animal page */ 
-router.get('/delete', animal_controlers.animal_delete_Page); 
+router.get('/delete',secured, animal_controlers.animal_delete_Page); 
 
 module.exports = router;
 
